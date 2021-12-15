18452c6f3bcde182c68a8276f06dbc0f64e1c3b0
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2021-12-15 11:02:29 +0100

    fix

--- samples/errors.py
+++
@@ -1,6 +0,0 @@
-# %%
-0 + "1"
-
-# %%
-raise Warning("🌧️  light rain")
-
--- samples/streams.py
+++
@@ -1,7 +0,0 @@
-# %%
-print("👋 Hello world! 🌍")
-
-# %%
-import sys
-print("🔥 This is fine. 🔥 (https://gunshowcomic.com/648)", file=sys.stderr)
-
